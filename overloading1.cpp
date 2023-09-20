b75d928439c0c1f01d32d9b56aebd0b3dc9baf37
nitinbhopale <nitinbhopale53@gmail.com>
2023-09-20 20:20:25 +0530

    cmt

---
+++ overloading1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class Demo
+{
+    public:
+        int Addition(int A, int B) //Addition@2ii
+        {
+            int Ans = 0;
+            Ans = A+B;
+            return Ans;
+        }
+        int Addition(int A, int B , int C) //Addition@3iii
+        {
+            int Ans = 0;
+            Ans = A+B+C;
+            return Ans;
+        }
+        float Addition(float A, float B) //Addition@2ff
+        {
+            float Ans = 0.0f;
+            Ans = A+B;
+            return Ans;
+        }
+    
+};
+   
+
+int main()
+{
+    Demo obj;
+
+    int i = 10 , j = 20 , k = 30;
+    int Ret = 0;
+    float fRet = 0.0f;
+    float a = 10.10, b = 88.23;
+
+    Ret = obj.Addition(i,j);
+
+    cout<<"Addition is : "<<Ret<<"\n";
+
+     Ret = obj.Addition(i,j,k);
+
+    cout<<"Addition is : "<<Ret<<"\n";
+
+    fRet = obj.Addition(a,b);
+
+    cout<<"Addition is :"<<fRet<<"\n";
+
+    return 0;
+}
