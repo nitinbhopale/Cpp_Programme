c09cb3dd209780214c2a3a38ef8379d3490fc05b
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-08 11:46:49 +0530

    cmt

--- ConstMethod.cpp
+++ ConstMethod.cpp
@@ -11,9 +11,26 @@ class Demo
         // Parametrised costructor
         Demo(int a, int b, int c) 
         {
-            i = a;
-            j = b;
-            k = c;
+            i = a;   j = b;  k = c;
+        }
+
+        void fun()
+        {
+            i++;
+            j++;
+            k++;
+        }
+        void gun(int x, const int y) const
+        {
+            int no1 = 10;
+            const int no2 = 20;
+            x++; //A
+            // y++; //NA
+            no1++; //A
+            // no2++; //NA
+            // i++; //NA
+            // j++; //NA
+           // k++; //NA
         }
 };
 
@@ -21,6 +38,10 @@ int main()
 {
     Demo obj(11,21,51);
 
+    obj.fun();
+    obj.gun(51,101);
+
+    const Demo obj2(11,21,51); 
     
     return 0;
 };
